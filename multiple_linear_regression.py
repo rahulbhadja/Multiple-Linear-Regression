466d64156f34ad77828a374b8ee8939556cb2d63
rahulbhadja <41661703+rahulbhadja@users.noreply.github.com>
2020-03-20 11:27:06 +0530

    Update multiple_linear_regression.py

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -10,23 +10,23 @@ dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
 
-# Encoding categorical data
+# categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder = LabelEncoder()
 X[:, 3] = labelencoder.fit_transform(X[:, 3])
 onehotencoder = OneHotEncoder(categorical_features = [3])
 X = onehotencoder.fit_transform(X).toarray()
 
-# Avoiding the Dummy Variable Trap
+# Avoiding  Dummy Variable 
 X = X[:, 1:]
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.model.selection import train_test_split
+# Splitting  dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 
 
-# Fitting Multiple Linear Regression to the Training set
+# Fitting Multiple Linear Regression 
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
